54e83bad4fc66480e7c6a9e220c9278b68ec962b
stef <stef.verve@gmail.com>
2016-12-14 12:04:45 -0500

    Initial Commit

---
+++ TechBooth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechBooth
+//
+//  Created by Stefan Verveniotis on 2016-12-14.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
