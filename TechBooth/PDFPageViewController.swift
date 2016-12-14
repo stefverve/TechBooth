b85a66b6c7952d05bbcd9dd9b56b72538a18796e
stef <stef.verve@gmail.com>
2016-12-14 15:21:15 -0500

    Storyboard outline

---
+++ TechBooth/PDFPageViewController.swift
@@ -0,0 +1,82 @@
+//
+//  PDFPageViewController.swift
+//  TechBooth
+//
+//  Created by Stefan Verveniotis on 2016-12-14.
+//
+//
+
+import UIKit
+
+class PDFPageViewController: UIPageViewController, UIPageViewControllerDataSource {
+    
+    var document: CGPDFDocument!
+    var pageCount = 0
+    var pageSize = CGSize(width: 0, height: 0)
+    
+    
+    
+ //   var orderedViewControllers: [SinglePageViewController]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // temp code to load pdf from bundle.main
+        let document: CGPDFDocument = CGPDFDocument(Bundle.main.url(forResource: "Fyi", withExtension: "pdf")! as CFURL)!
+        
+        self.document = document
+        self.pageCount = document.numberOfPages
+        self.pageSize = (document.page(at: 1)?.getBoxRect(CGPDFBox.mediaBox).size)!
+        
+//        for page in 1...self.pageCount {
+//        
+//        return [self.newColoredViewController(color: "Green"),
+//                self.newColoredViewController(color: "Red"),
+//                self.newColoredViewController(color: "Blue")]
+//        }
+        dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    
+//    private(set) lazy var orderedViewControllers: [SinglePageViewController] = {
+//        for page in 1...
+//        
+//        return [self.newColoredViewController("Green"),
+//                self.newColoredViewController("Red"),
+//                self.newColoredViewController("Blue")]
+//    }()
+    
+    
+    
+    private func newColoredViewController(color: String) -> SinglePageViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: "pdfPage") as! SinglePageViewController
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TechBooth/pdfView.swift
@@ -0,0 +1,59 @@
+//
+//  pdfView.swift
+//  TechBooth
+//
+//  Created by Stefan Verveniotis on 2016-12-14.
+//
+//
+
+import UIKit
+
+class pdfView: UIView {
+
+    
+    
+//    - (void)drawRect:(CGRect)rect
+//    {
+//    CGContextRef ctx = UIGraphicsGetCurrentContext();
+//    
+//    // PDF might be transparent, assume white paper
+//    [[UIColor whiteColor] set];
+//    CGContextFillRect(ctx, rect);
+//    
+//    // Flip coordinates
+//    CGContextGetCTM(ctx);
+//    CGContextScaleCTM(ctx, 1, -1);
+//    CGContextTranslateCTM(ctx, 0, -rect.size.height);
+//    
+//    // url is a file URL
+//    CGPDFDocumentRef pdf = CGPDFDocumentCreateWithURL((CFURLRef)url);
+//    CGPDFPageRef page1 = CGPDFDocumentGetPage(pdf, 1);
+//    
+//    // get the rectangle of the cropped inside
+//    CGRect mediaRect = CGPDFPageGetBoxRect(page1, kCGPDFCropBox);
+//    CGContextScaleCTM(ctx, rect.size.width / mediaRect.size.width,
+//    rect.size.height / mediaRect.size.height);
+//    CGContextTranslateCTM(ctx, -mediaRect.origin.x, -mediaRect.origin.y);
+//    
+//    // draw it
+//    CGContextDrawPDFPage(ctx, page1);
+//    CGPDFDocumentRelease(pdf);
+//    }
+//    
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        let ctx = UIGraphicsGetCurrentContext()
+        ctx?.setFillColor(UIColor.white.cgColor)
+        ctx?.fill(rect)
+        
+        ctx?.scaleBy(x: 1, y: -1)
+        ctx?.translateBy(x: 0, y: -rect.size.height)
+        
+        let mediaRect = 
+        
+    }
+    
+
+}
