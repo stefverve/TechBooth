70ecf3d677f8f1221698e8ffe67b381874a1eed9
stef <stef.verve@gmail.com>
2016-12-14 20:51:41 -0500

    oops

--- TechBooth/MainMenuView.swift
+++ TechBooth/MainMenuView.swift
@@ -14,6 +14,7 @@ class MainMenuView: UIViewController {
     @IBAction func importPDFButton(_ sender: UIButton) {
     }
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
