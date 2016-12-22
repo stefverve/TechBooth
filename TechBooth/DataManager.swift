deaefd2e3e0f231d42adadeae7da950f75e2f429
guiworm <dylan.mccrindle@gmail.com>
2016-12-21 21:09:41 -0500

    STUPID COMMIT

--- TechBooth/DataManager.swift
+++ TechBooth/DataManager.swift
@@ -213,7 +213,7 @@ class DataManager {
 		for annotation in annotationsOfType {
 			if (annotation.dotY == changeAnnot.dotY){
 				changeAnnot.cueNum = Int16(count)
-				for index in count..<annotationsOfType.count{
+				for index in count-1..<annotationsOfType.count{
 					annotationsOfType[index].cueNum = Int16(count)
 					count += 1
 				}
